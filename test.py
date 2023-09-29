1fa2c2c331cca2ff292818fbebbf933fd223213d
nonNoise <kitagami@artifactnoise.com>
2023-09-30 07:59:07 +0900

    add

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 from bottle import route, run
 
-@route('/hello')
-def hello():
+@route('/')
+def root():
     return "Hello World!"
+
+@route('/camera')
+def camera():
+    from picamera2 import Picamera2
+    picam2 = Picamera2()
+    #カメラ画像を保存する
+    picam2.start_and_capture_file("test.jpg")
+    return "Hello World!"
+
 
 run(host='192.168.100.9', port=8080, debug=True)
