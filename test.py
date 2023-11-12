7bba236bf27f6356785ae80e5ff40b6c9ee063fb
nonNoise <kitagami@artifactnoise.com>
2023-11-12 14:55:15 +0900

    add

--- test.py
+++ test.py
@@ -1,15 +1,19 @@
 from picamera2 import Picamera2, Preview
 import time
+import json
 
 picam2 = Picamera2()
 camera_config = picam2.create_preview_configuration()
 picam2.configure(camera_config)
 print(camera_config)
+with open('camera_config.json', 'w') as f:
+    json.dump(camera_config, f, ensure_ascii=False)
+
 picam2.start_preview(Preview.QTGL)
 picam2.start()
 time.sleep(2)
 while 1:
     picam2.capture_file("test.jpg")
-    time.sleep(2)
+    time.sleep(1)
 
-
+    
