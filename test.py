ac605ea8388bcff7cc86bf586de663484f2fe246
nonNoise <kitagami@artifactnoise.com>
2023-11-12 14:49:42 +0900

    add

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 from picamera2 import Picamera2, Preview
 picam2 = Picamera2()
-#picam2.start_preview(Preview.NULL)
-picam2.start_and_capture_file("test.jpg")
+camera_config = picam2.create_preview_configuration()
+picam2.configure(camera_config)
+print(camera_config)
+picam2.start_preview(Preview.DRM)
+picam2.start()
+time.sleep(2)
+picam2.capture_file("test.jpg")
 
