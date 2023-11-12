95da2267acc40cc1b8a78e3a89802f45f11d48e4
nonNoise <kitagami@artifactnoise.com>
2023-11-12 15:02:47 +0900

    add

--- test.py
+++ test.py
@@ -6,14 +6,46 @@ picam2 = Picamera2()
 camera_config = picam2.create_preview_configuration()
 picam2.configure(camera_config)
 print(camera_config)
-with open('camera_config.json', 'w') as f:
-    json.dump(camera_config, f, ensure_ascii=False)
 
 picam2.start_preview(Preview.QTGL)
 picam2.start()
 time.sleep(2)
+
+gain =0.0
 while 1:
     picam2.capture_file("test.jpg")
     time.sleep(1)
+    picam2.set_controls({"ExposureTime": 10000, "AnalogueGain": gain})
+    gain = gain + 0.1
 
-    
+
+
+"""
+{
+ 'use_case': 'preview', 
+ 'transform': <libcamera.Transform 'identity'>,
+  'colour_space': <libcamera.ColorSpace 'sYCC'>, 
+  'buffer_count': 4, 
+  'queue': True, 
+    'main': {
+        'format': 'XBGR8888', 
+        'size': (640, 480), 
+        'stride': 2560, 
+        'framesize': 1228800
+    }, 
+    'lores': None, 
+    'raw': {
+        'format': 'SBGGR10_CSI2P', 
+        'size': (1456, 1088), 
+        'stride': 1824, 
+        'framesize': 1984512
+    }, 
+    'controls': {
+        'NoiseReductionMode': <NoiseReductionModeEnum.Minimal: 3>, 
+        'FrameDurationLimits': (100, 83333)
+    }, 
+    'sensor': {}, 
+    'display': 'main', 
+    'encode': 'main'
+}
+"""
