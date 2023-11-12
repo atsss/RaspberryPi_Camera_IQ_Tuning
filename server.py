369b4cc5b69f71c2161e5ad91762aa5e35b7e8ff
nonNoise <kitagami@artifactnoise.com>
2023-11-12 15:38:39 +0900

    add

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from bottle import run, route, template , static_file
+import time
 
 @route('/')
 def index():
@@ -9,7 +10,9 @@ def camera(filename):
     #pos = filename.find('?')
     #filename = filename[:pos]
     print(filename)
-    picam2.start_and_capture_file("./img/"+filename)
+    picam2.capture_file("./img/"+filename)
+
+    #picam2.start_and_capture_file("./img/"+filename)
     return static_file(filename, root='./img/')
 
 @route('/img/<filename>')
@@ -20,5 +23,8 @@ def show_img(filename):
 from picamera2 import Picamera2, Preview
 picam2 = Picamera2()
 picam2.start_preview(Preview.NULL)
+picam2.start()
+time.sleep(2)
+
 
 run(host='0.0.0.0', port=8080, debug=True)
