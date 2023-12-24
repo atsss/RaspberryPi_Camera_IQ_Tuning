c3755a2f8b3d4d786cf1e8ff852e4f4d08828ef1
atsss <atsushi0623itoh@gmail.com>
2023-12-24 05:10:41 +0000

    Remove colour import

--- PC_check/color_accuracy_V2.py
+++ PC_check/color_accuracy_V2.py
@@ -1,12 +1,11 @@
 import cv2
-import colour
 from PIL import Image
 import numpy as np
 import time
 import os, sys
 from colormath.color_diff import delta_e_cie2000 , delta_e_cie1976
-from colormath.color_objects import sRGBColor ,LabColor 
-from colormath.color_conversions import convert_color 
+from colormath.color_objects import sRGBColor ,LabColor
+from colormath.color_conversions import convert_color
 
 print(np.__version__)
 print(cv2.__version__)
@@ -20,14 +19,14 @@ IMX_FILE_NAME = 'image_json.jpg'
 
 def plot_rgbl(img_rgbl, box_size = (25, 25)):
     nb_boxes = img_rgbl.shape[0]
-    
+
     if len(img_rgbl.shape) == 3:
         clrs = np.ascontiguousarray(np.tile(img_rgbl[:, 0, 0:3].reshape(nb_boxes, 3, 1, 1), (box_size[0], box_size[1])).transpose(0, 2, 3, 1))
     elif len(img_rgbl.shape) == 2:
         clrs = np.ascontiguousarray(np.tile(img_rgbl[:, 0:3].reshape(nb_boxes, 3, 1, 1), (box_size[0], box_size[1])).transpose(0, 2, 3, 1))
     else:
         raise ValueError
-    
+
     clrs.resize((nb_boxes*box_size[0], box_size[1], 3))
     clrs = clrs.transpose(1, 0, 2)
     clrs = np.clip(clrs * 255, 0, 255).astype('uint8')
@@ -49,28 +48,28 @@ def plot_rgblx2(img_rgbl_1, img_rgbl_2 ,box_size = (25, 25)):
 
 def crop_center(image, crop_width, crop_height):
     width, height = image.size
-    
+
     # トリミングする領域の左上の座標を計算
     # left = int((width - crop_width) / 2)
     left = int((width - crop_width) / 2 - 50)
     # top = int((height - crop_height) / 2)
     top = int((height - crop_height) / 2 - 50)
-    
+
     # 画像をトリミング
     cropped_image = image.crop((left, top, left + crop_width, top + crop_height))
-    
+
     return cropped_image
 
 def rgb_to_lab(rgb_normalized):
     # RGB の値を 0-1 の範囲に正規化
     # rgb_normalized = [channel / 255.0 for channel in rgb]
-    
+
     # RGB を XYZ に変換
     xyz = colour.sRGB_to_XYZ(rgb_normalized)
-    
+
     # XYZ を Lab に変換
     lab = colour.XYZ_to_Lab(xyz)
-    
+
     return lab
 def colormath_rgb2lab(rgb):
     return convert_color(sRGBColor(rgb[0][0],rgb[0][1],rgb[0][2]), LabColor, target_illuminant='d65')
@@ -117,40 +116,40 @@ for ilumination in ILUMINATIONS:
     srcs = []
     for i, device in enumerate(DEVICES):
         img = np.array(IMGS[ilumination][device]).copy()
-        
+
         # Detect Color CHart and extract checker
         detector = cv2.mcc.CCheckerDetector_create()
         succ = detector.process(img, chartType=cv2.mcc.MCC24, nc=1, useNet=True)
         assert succ
         checker = detector.getBestColorChecker()
         CHARTS[ilumination][device] = checker
-        
+
         # Process checker to extract colors
         chartsRGB = checker.getChartsRGB()
         src_rgb = chartsRGB[:, 1].copy()
-    
+
         # Bottom left corner is assumed to be WHITE
         src_rgb = np.round(src_rgb, 0).astype('uint8')
         src_rgb.resize(24, 3)
         src_rgb = src_rgb[::-1, ::-1]
         src = (src_rgb.copy()/255)[:, np.newaxis, :]
-        
+
         # Check direction
         if src[0].mean() < src[-1].mean():
             src = src[::-1]
         CHECKER_SRCS[ilumination][device] = src
-        
+
         # Keep to plat later
         srcs.append(src)
-        
+
         # Draw
         draw = cv2.mcc.CCheckerDraw_create(checker, color=(0, 255, 0), thickness=2)
         draw.draw(img)
-        
+
         comp_img[:, IMG_SIZE[0]*i:IMG_SIZE[0]*(i+1), :] = img
     #display(Image.fromarray(comp_img))
     Image.fromarray(comp_img).show()
-    
+
     # Plot colors
     #for src in srcs:
         #display(Image.fromarray(plot_rgbl(src.copy(), (50, 50))))
@@ -182,7 +181,7 @@ for j in range(columns):
     #deltE = np.sqrt(((target_lab - tuned_lab) ** 2).sum())
     #deltE = delta_e_cie1976(colormath_rgb2lab(normalized_rgbs[GOPRO_INDEX][j]), colormath_rgb2lab(normalized_rgbs[IMX_INDEX][j]))
     deltE = delta_e_cie2000(colormath_rgb2lab(normalized_rgbs[GOPRO_INDEX][j]), colormath_rgb2lab(normalized_rgbs[IMX_INDEX][j]))
-    
+
     print(f'{labels[j]}: {deltE}')
     deltEs[j] = deltE
 
--- PC_check/color_checker_V2.py
+++ PC_check/color_checker_V2.py
@@ -1,5 +1,4 @@
 import cv2
-import colour
 from PIL import Image
 import numpy as np
 import time
@@ -7,14 +6,12 @@ import math
 import os, sys
 from colormath.color_diff import delta_e_cie2000 , delta_e_cie1976
 from colormath.color_objects import sRGBColor ,LabColor ,XYZColor
-from colormath.color_conversions import convert_color 
+from colormath.color_conversions import convert_color
 import numpy as np
-import colour
 
 
 print(np.__version__)
 print(cv2.__version__)
-#print(colour.__version__)
 
 
 IMG_SIZE = ((int)(3280/1.5), (int)(2464/1.5))
@@ -24,14 +21,14 @@ GOPR_FILE_NAME = 'GOPR0075.jpg'
 
 def plot_rgbl(img_rgbl, box_size = (25, 25)):
     nb_boxes = img_rgbl.shape[0]
-    
+
     if len(img_rgbl.shape) == 3:
         clrs = np.ascontiguousarray(np.tile(img_rgbl[:, 0, 0:3].reshape(nb_boxes, 3, 1, 1), (box_size[0], box_size[1])).transpose(0, 2, 3, 1))
     elif len(img_rgbl.shape) == 2:
         clrs = np.ascontiguousarray(np.tile(img_rgbl[:, 0:3].reshape(nb_boxes, 3, 1, 1), (box_size[0], box_size[1])).transpose(0, 2, 3, 1))
     else:
         raise ValueError
-    
+
     clrs.resize((nb_boxes*box_size[0], box_size[1], 3))
     clrs = clrs.transpose(1, 0, 2)
     clrs = np.clip(clrs * 255, 0, 255).astype('uint8')
@@ -54,37 +51,37 @@ def plot_rgblx2(img_rgbl_1, img_rgbl_2 ,box_size = (25, 25)):
 
 def crop_center(image, crop_width, crop_height):
     width, height = image.size
-    
+
     # トリミングする領域の左上の座標を計算
     # left = int((width - crop_width) / 2)
     left = int((width - crop_width) / 2 - 50)
     # top = int((height - crop_height) / 2)
     top = int((height - crop_height) / 2 - 50)
-    
+
     # 画像をトリミング
     cropped_image = image.crop((left, top, left + crop_width, top + crop_height))
-    
+
     return cropped_image
 
 def rgb_to_lab(rgb_normalized):
     # RGB の値を 0-1 の範囲に正規化
     # rgb_normalized = [channel / 255.0 for channel in rgb]
-    
+
     # RGB を XYZ に変換
     xyz = colour.sRGB_to_XYZ(rgb_normalized)
-    
+
     # XYZ を Lab に変換
     lab = colour.XYZ_to_Lab(xyz)
-    
+
     return lab
 def colormath_rgb2lab(rgb):
     return convert_color(sRGBColor(rgb[0][0],rgb[0][1],rgb[0][2]), LabColor, target_illuminant='d65')
 
 def plot_rgb_chart(rgbl, box_size = (25, 25)):
     nb_boxes = rgbl.shape[0]
-    
+
     clrs = np.ascontiguousarray(np.tile(rgbl[:, 0:3].reshape(nb_boxes, 3, 1, 1), (box_size[0], box_size[1])).transpose(0, 2, 3, 1))
-    
+
     clrs.resize((nb_boxes*box_size[0], box_size[1], 3))
     clrs = clrs.transpose(1, 0, 2)
     clrs = np.clip(clrs * 255, 0, 255).astype('uint8')
@@ -166,7 +163,7 @@ def creat_ccm_2(deltaRGBs):
     r_offset = 0.155
     g_offset = 0.185
     b_offset = 0.205
-    
+
     print(np.amin(deltaRGBs[...,0]))
     sumR = 1+ (deltaRGBs[...,0].sum()/24)
     sumG = 1+ (deltaRGBs[...,1].sum()/24)
@@ -193,7 +190,7 @@ def creat_ccm_2(deltaRGBs):
     g_b = sumG*g_gain  + cnt_offset + ctt_g
     g_a = r_offset - cnt_offset/2  # + np.amax(deltaRGBs[...,0])
     g_c = b_offset - cnt_offset/2 # + np.amax(deltaRGBs[...,2])
-  
+
     b_c = sumB*b_gain + cnt_offset  + ctt_b
     b_a = r_offset - cnt_offset/2 # + np.amax(deltaRGBs[...,0])
     b_b = g_offset - cnt_offset/2 # + np.amax(deltaRGBs[...,1])
@@ -238,40 +235,40 @@ for ilumination in ILUMINATIONS:
     srcs = []
     for i, device in enumerate(DEVICES):
         img = np.array(IMGS[ilumination][device]).copy()
-        
+
         # Detect Color CHart and extract checker
         detector = cv2.mcc.CCheckerDetector_create()
         succ = detector.process(img, chartType=cv2.mcc.MCC24, nc=1, useNet=True)
         assert succ
         checker = detector.getBestColorChecker()
         CHARTS[ilumination][device] = checker
-        
+
         # Process checker to extract colors
         chartsRGB = checker.getChartsRGB()
         src_rgb = chartsRGB[:, 1].copy()
-    
+
         # Bottom left corner is assumed to be WHITE
         src_rgb = np.round(src_rgb, 0).astype('uint8')
         src_rgb.resize(24, 3)
         src_rgb = src_rgb[::-1, ::-1]
         src = (src_rgb.copy()/255)[:, np.newaxis, :]
-        
+
         # Check direction
         if src[0].mean() < src[-1].mean():
             src = src[::-1]
         CHECKER_SRCS[ilumination][device] = src
-        
+
         # Keep to plat later
         srcs.append(src)
-        
+
         # Draw
         draw = cv2.mcc.CCheckerDraw_create(checker, color=(0, 255, 0), thickness=2)
         draw.draw(img)
-        
+
         comp_img[:, IMG_SIZE[0]*i:IMG_SIZE[0]*(i+1), :] = img
     #display(Image.fromarray(comp_img))
     Image.fromarray(comp_img).show()
-    
+
     # Plot colors
     #for src in srcs:
         #display(Image.fromarray(plot_rgbl(src.copy(), (50, 50))))
@@ -322,40 +319,40 @@ for ilumination in ILUMINATIONS:
     srcs = []
     for i, device in enumerate(DEVICES):
         img = np.array(IMGS[ilumination][device]).copy()
-        
+
         # Detect Color CHart and extract checker
         detector = cv2.mcc.CCheckerDetector_create()
         succ = detector.process(img, chartType=cv2.mcc.MCC24, nc=1, useNet=True)
         assert succ
         checker = detector.getBestColorChecker()
         CHARTS[ilumination][device] = checker
-        
+
         # Process checker to extract colors
         chartsRGB = checker.getChartsRGB()
         src_rgb = chartsRGB[:, 1].copy()
-    
+
         # Bottom left corner is assumed to be WHITE
         src_rgb = np.round(src_rgb, 0).astype('uint8')
         src_rgb.resize(24, 3)
         src_rgb = src_rgb[::-1, ::-1]
         src = (src_rgb.copy()/255)[:, np.newaxis, :]
-        
+
         # Check direction
         if src[0].mean() < src[-1].mean():
             src = src[::-1]
         CHECKER_SRCS[ilumination][device] = src
-        
+
         # Keep to plat later
         srcs.append(src)
-        
+
         # Draw
         draw = cv2.mcc.CCheckerDraw_create(checker, color=(0, 255, 0), thickness=2)
         draw.draw(img)
-        
+
         comp_img[:, IMG_SIZE[0]*i:IMG_SIZE[0]*(i+1), :] = img
     #display(Image.fromarray(comp_img))
     Image.fromarray(comp_img).show()
-    
+
     # Plot colors
     sRGB_Color = np.zeros((24, 3))
     for j in range(24):
@@ -390,8 +387,8 @@ for j in range(columns):
     #print(normalized_rgbs[0][j][0])
     chack_in_A = convert_color(sRGBColor(color_checker_rgb[j][0]/255,color_checker_rgb[j][1]/255,color_checker_rgb[j][2]/255), LabColor)
     chack_in_B = convert_color(sRGBColor(normalized_rgbs[0][j][0][0],normalized_rgbs[0][j][0][1],normalized_rgbs[0][j][0][2]), LabColor)
-             
-    deltE = delta_e_cie2000(chack_in_A,chack_in_B )    
+
+    deltE = delta_e_cie2000(chack_in_A,chack_in_B )
     print(f'{labels[j]}: {deltE}')
     deltEs[j] = deltE
 # Plot colors
@@ -399,10 +396,10 @@ print(f'Total: {deltEs.sum()}')
 
 deltaRGBs = np.zeros((24, 3))
 rgb_table = np.zeros((24, 3))
-for j in range(24):        
+for j in range(24):
     chack_in_A = convert_color(sRGBColor(color_checker_rgb[j][0],color_checker_rgb[j][1],color_checker_rgb[j][2]), sRGBColor)
     chack_in_B = convert_color(sRGBColor(normalized_rgbs[0][j][0][0],normalized_rgbs[0][j][0][1],normalized_rgbs[0][j][0][2]), sRGBColor)
-                                
+
     deltaRGB = [chack_in_A.rgb_r - chack_in_B.rgb_r,
                         chack_in_A.rgb_g - chack_in_B.rgb_g,
                         chack_in_A.rgb_b - chack_in_B.rgb_b]
@@ -414,14 +411,14 @@ for j in range(24):
 Image.fromarray(plot_rgb_chart(deltaRGBs, (50, 50))).show()
 #print(deltaRGBs)
 
-for j in range(24):        
+for j in range(24):
     chack_in_A = convert_color(sRGBColor(color_checker_rgb[j][0]/255,color_checker_rgb[j][1]/255,color_checker_rgb[j][2]/255), sRGBColor)
     chack_in_B = convert_color(sRGBColor(normalized_rgbs[0][j][0][0],normalized_rgbs[0][j][0][1],normalized_rgbs[0][j][0][2]), sRGBColor)
-           
+
     chack_in_B.rgb_r = chack_in_B.rgb_r + deltaRGBs[j][0]
     chack_in_B.rgb_g = chack_in_B.rgb_g + deltaRGBs[j][1]
     chack_in_B.rgb_b = chack_in_B.rgb_b + deltaRGBs[j][2]
-    
+
     tmp = convert_color(chack_in_B, sRGBColor)
     rgb_table[j] = [chack_in_B.rgb_r,chack_in_B.rgb_g,chack_in_B.rgb_b]
 
@@ -431,13 +428,11 @@ for j in range(24):
     print(f'{labels[j]}: {deltE}')
     deltEs[j] = deltE
 print(f'Total: {deltEs.sum()}')
-#Image.fromarray(plot_rgb_chart(deltaRGBs, (50, 50))).show()
 
 
 print(deltaRGBs)
 
 
-#ccm = creat_ccm(normalized_rgbs[0],color_checker_rgb)
 ccm = creat_ccm_2(deltaRGBs)
 
 print("-------------------------------------------")
